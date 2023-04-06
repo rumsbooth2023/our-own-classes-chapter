c18b1fc16ebc01bccafd2fabdca668183c5c9571
rumsbooth baddam <rbaddam@chicagobooth.edu>
2023-04-06 04:00:07 +0000

    Completed

--- class_person.rb
+++ class_person.rb
@@ -6,8 +6,28 @@
 #  - An instance method, full_name, that puts first_name and last_name together
 #  - An instance method, age, that calculates the number of years between today and birthdate
 
+
 class Person
-  
+  require("date") # We need to pull in the Date class, which is not loaded by default
+
+  attr_accessor :first_name
+  attr_accessor :last_name
+  attr_accessor :birthdate
+
+  def age
+    dob = Date.parse(self.birthdate)
+    now = Date.today
+    age_in_days = now - dob
+    age_in_years = age_in_days / 365
+
+    return age_in_years.to_i
+  end
+
+  def full_name
+    full_name = first_name + " " + last_name
+    return full_name
+  end
+
 end
 
 # Test your instance methods by adding to the end of this file:
